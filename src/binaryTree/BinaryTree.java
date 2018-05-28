0907d0e1fbd792393e33eb0fc284f77769eeaca0
trichromatic13 <vibhash18@gmail.com>
2018-05-28 22:46:22 +0530

    Tree added

---
+++ src/binaryTree/BinaryTree.java
@@ -0,0 +1,16 @@
+package binaryTree;
+
+public class BinaryTree {
+	
+	Node root;
+	
+	BinaryTree()
+	{
+		root = null;
+	}
+	
+	BinaryTree(int val)
+	{
+		root = new Node(val);
+	}
+}
---
+++ src/binaryTree/Node.java
@@ -0,0 +1,12 @@
+package binaryTree;
+
+public class Node {
+	int data;
+	Node left, right;
+	
+	public Node(int value)
+	{
+		data = value;
+		left = right = null;
+	}
+}
